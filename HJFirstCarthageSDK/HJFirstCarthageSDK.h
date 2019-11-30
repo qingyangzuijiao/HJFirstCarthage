be32e408d35c18422bd5650898e60e593a0618c5
qingyangzuijiao <2463032878@qq.com>
2019-11-30 14:32:13 +0800

    生成HJFirstCarthageSDK

---
+++ HJFirstCarthageSDK/HJFirstCarthageSDK.h
@@ -0,0 +1,19 @@
+//
+//  HJFirstCarthageSDK.h
+//  HJFirstCarthageSDK
+//
+//  Created by whj on 2019/11/30.
+//  Copyright © 2019 whj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HJFirstCarthageSDK.
+FOUNDATION_EXPORT double HJFirstCarthageSDKVersionNumber;
+
+//! Project version string for HJFirstCarthageSDK.
+FOUNDATION_EXPORT const unsigned char HJFirstCarthageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HJFirstCarthageSDK/PublicHeader.h>
+
+
---
+++ HJFirstCarthageSDK/HJFristCarthageMain.swift
@@ -0,0 +1,19 @@
+//
+//  HJFristCarthageMain.swift
+//  HJFristCarthageSDK
+//
+//  Created by whj on 2019/11/30.
+//  Copyright © 2019 whj. All rights reserved.
+//
+
+import Foundation
+
+public class HJFristCarthageMain {
+    
+    public func hj_summary() {
+        
+        print("很荣幸见到你，这是whj的第一个carthage库")
+        
+    }
+
+}
